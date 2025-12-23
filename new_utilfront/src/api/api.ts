7921275479f3df6957305ac47cf2ad166a98a4ca
Ibnul Mohammad Adib <adib.bdhk@gmail.com>
2025-12-24 01:34:09 +0600

    / added

--- new_utilfront/src/api/api.ts
+++ new_utilfront/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://util-task.onrender.com"
+    baseURL: "https://util-task.onrender.com/"
 });
 
 api.interceptors.request.use((config) => {
